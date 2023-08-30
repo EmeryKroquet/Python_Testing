1668d36ad8f249e230b7911bd2f2fc55e8e4f41f
EmeryKroquet <emery.kroquet@gmail.com>
2023-08-30 20:42:59 +0200

    Fix email bug in showSummary function

--- server.py
+++ server.py
@@ -26,8 +26,13 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
+    email = request.form['email']
+    club = next((club for club in clubs if club['email'] == email), None)
+    if club is None:
+        flash("Email not found")
+        return redirect(url_for('index'))
     return render_template('welcome.html',club=club,competitions=competitions)
+
 
 
 @app.route('/book/<competition>/<club>')
