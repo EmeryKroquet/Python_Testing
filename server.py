3ab113b8c2a2499918301cb2399e6a786e5986d7
EmeryKroquet <emery.kroquet@gmail.com>
2023-09-06 01:32:00 +0200

    fix show summary index bug

--- server.py
+++ server.py
@@ -16,18 +16,25 @@ def loadCompetitions():
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
+app.debug = True
 
 competitions = loadCompetitions()
 clubs = loadClubs()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST'])
+
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    # Correction 2: former code didn't handle wrong email input --> exception handler
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+        return render_template('welcome.html', club=club, competitions=competitions)
+    except IndexError:
+        return redirect('/')
 
 
 @app.route('/book/<competition>/<club>')
@@ -57,3 +64,8 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+
+# Correction 1: code below was missing --> server couldn't be launched
+if __name__ == "__main__":
+    app.run()
